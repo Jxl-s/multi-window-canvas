3efc8ee8c9340ed95509ec42127f8e16d17ba7b1
j <77560326+Jxl-s@users.noreply.github.com>
2024-03-06 21:22:29 -0500

    simpler uuid

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-const SESSION_UUID = (() => {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-        /[xy]/g,
-        function (c) {
-            var r = (Math.random() * 16) | 0,
-                v = c == "x" ? r : (r & 0x3) | 0x8;
-            return v.toString(16);
-        }
-    );
-})();
+const SESSION_UUID = Math.random().toString();
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
